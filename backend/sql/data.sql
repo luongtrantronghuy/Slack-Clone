3c8ec4f6fa963b307834f2c5ef4669a0465d97d5
luongtrantronghuy <39868850+luongtrantronghuy@users.noreply.github.com>
2021-07-15 21:55:51 -0700

    removed thread for testing

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -10,6 +10,6 @@
 
 DELETE FROM channels;
 -- Populate Your Tables Here --
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "First message in the asgn1 channel!","from": "Adam","to": "","sent-at": "2020-11-17T23:17:19Z"}', '[{"content": "First reply in the thread!","from": "Eve","to": "","sent-at": "2020-11-17T23:17:19Z"}]');
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "First message in the asgn2 channel!","from": "Eve","to": "","sent-at": "2020-10-17T23:17:19Z"}', '[{"content": "First reply in the thread!","from": "Adam","to": "","sent-at": "2020-10-17T23:17:19Z"}]');
-INSERT INTO channels(channel, messages, thread) VALUES ('dm','{"content": "Hi Adam, this is Eve!","from": "Eve","to": "Adam","sent-at": "2020-10-17T23:17:19Z"}', '[{"content": "Hey Eve!","from": "Adam","to": "Eve","sent-at": "2020-10-17T23:17:19Z"}]');
+INSERT INTO channels(channel, messages) VALUES ('assignment-1','{"content": "First message in the asgn1 channel!","from": "Adam","to": "","sent-at": "2020-11-17T23:17:19Z"}');
+INSERT INTO channels(channel, messages) VALUES ('assignment-2','{"content": "First message in the asgn2 channel!","from": "Eve","to": "","sent-at": "2020-10-17T23:17:19Z"}');
+INSERT INTO channels(channel, messages) VALUES ('dm','{"content": "Hi Adam, this is Eve!","from": "Eve","to": "Adam","sent-at": "2020-10-17T23:17:19Z"}');
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS channels;
 -- Your database schema goes here --
-CREATE TABLE channels(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), channel VARCHAR(32), messages jsonb, thread jsonb[]);
+CREATE TABLE channels(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), channel VARCHAR(32), messages jsonb);
