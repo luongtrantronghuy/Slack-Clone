0c12e3b871225f76970c45c7b7d274d67d6a94f4
luongtrantronghuy <39868850+luongtrantronghuy@users.noreply.github.com>
2021-07-16 21:36:46 -0700

    changed sent-at to sent

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -7,14 +7,14 @@
 --   "content": "string"
 --   "from": "string"
 --   "to": "string"
---   "sent-at": "2020-11-17T23:17:19Z"
+--   "sent": "2020-11-17T23:17:19Z"
 -- }
 
 DELETE FROM channels;
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "First message in the asgn1 channel!","from": "Molly Member","to": "","sent-at": "2020-11-17T23:17:19Z"}', ARRAY ['{"content": "First message in all threads!","from": "Anna Admin","to": "","sent-at": "2020-11-17T23:17:19Z"}', '{"content": "Second message in the thread!","from": "Anna Admin","to": "","sent-at": "2021-1-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "Second message in the asgn1 channel!","from": "Anna Admin","to": "","sent-at": "2021-1-17T23:17:19Z"}', ARRAY ['{"content": "This message can have threads too!","from": "Anna Admin","to": "","sent-at": "2021-1-17T23:17:19Z"}', '{"content": "Yes","from": "Molly Member","to": "","sent-at": "2021-1-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "First message in the asgn2 channel!","from": "Anna Admin","to": "","sent-at": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "Thread in assignment-2!","from": "Molly Member","to": "","sent-at": "2020-10-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('dm','{"content": "Hi Molly Member, this is Anna Admin!","from": "Anna Admin","to": "Molly Member","sent-at": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "hey from thread!","from": "Molly Member","to": "Anna Admin","sent-at": "2020-11-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "First message in the asgn1 channel!","from": "Molly Member","to": "","sent": "2020-11-17T23:17:19Z"}', ARRAY ['{"content": "First message in all threads!","from": "Anna Admin","to": "","sent-at": "2020-11-17T23:17:19Z"}', '{"content": "Second message in the thread!","from": "Anna Admin","to": "","sent-at": "2021-1-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "Second message in the asgn1 channel!","from": "Anna Admin","to": "","sent": "2021-1-17T23:17:19Z"}', ARRAY ['{"content": "This message can have threads too!","from": "Anna Admin","to": "","sent-at": "2021-1-17T23:17:19Z"}', '{"content": "Yes","from": "Molly Member","to": "","sent-at": "2021-1-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "First message in the asgn2 channel!","from": "Anna Admin","to": "","sent": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "Thread in assignment-2!","from": "Molly Member","to": "","sent-at": "2020-10-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('dm','{"content": "Hi Molly Member, this is Anna Admin!","from": "Anna Admin","to": "Molly Member","sent": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "hey from thread!","from": "Molly Member","to": "Anna Admin","sent-at": "2020-11-17T23:17:19Z"}']);
 
 -- User Table --
 -- {
