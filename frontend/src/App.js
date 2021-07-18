e5f997ee083bb4292385519a5b4a422d9f938d07
Chris Llamas <chrisllamas2000@gmail.com>
2021-07-18 20:45:51 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,3 @@
-import React from 'react';
-
-import Emoji from './Emoji';
-
 /**
  * Forcing icons to be smaller than small:
  * https://stackoverflow.com/questions/37480517/floating-action-button-is-there-a-straightforward-way-to-make-a-button-smaller
