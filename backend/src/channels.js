de467687a438e431d748fab36e41d5ad92ea6b4b
luongtrantronghuy <39868850+luongtrantronghuy@users.noreply.github.com>
2021-07-20 15:52:46 -0700

    some small fixes to backend (grab messages)

--- backend/src/channels.js
+++ backend/src/channels.js
@@ -40,10 +40,57 @@ exports.sendMessage = async (req, res) => {
   }
 };
 
+function orderMessages(messages) {
+  const sorted = messages.sort((a,b) => new Date(a.sent) - new Date(b.sent));
+  const ordered = [];
+  let newTime = '';
+  for (message of sorted) {
+    newTime = '';
+    const time = new Date(message.sent);
+    const currDate = new Date();
+    const day = time.getDate();
+    const month = time.getMonth();
+    const year = time.getFullYear();
+    const months = ['Jan', 'Feb', 'Mar', 'Apr',
+      'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+      'Nov', 'Dec'];
+    if (day === currDate.getDate() &&
+      month === currDate.getMonth() &&
+      year === currDate.getFullYear()) {
+      newTime = 'Today';
+    } else if (year === currDate.getFullYear()) {
+      newTime = months[month] + ' ' + day;
+    } else {
+      newTime = months[month] + ' ' + year.toString();
+    }
+    if (ordered.length == 0) {
+      ordered.push({time: newTime, messages: [message]});
+    } else {
+      let found = false;
+      for (obj of ordered) {
+        if (newTime === obj.time) {
+          obj.messages.push(message);
+          found = true;
+          break;
+        }
+      }
+      if (!found){
+        ordered.push({time: newTime, messages: [message]});
+      }
+    }
+  }
+  if (ordered.length > 0) {
+    return ordered;
+  } else {
+    return undefined;
+  }
+};
+
 exports.getMessage = async (req, res) => {
   const messages = await db.getMessage(req.params.channel);
-  if (messages.length > 0) {
-    res.status(200).json(messages);
+  const ordered = orderMessages(messages);
+  if (ordered) {
+    res.status(200).json(ordered);
   } else {
     res.status(404).send();
   }
