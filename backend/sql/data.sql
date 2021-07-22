8b12e063b751a3ff6f8f5af0ead96db64ab914db
luongtrantronghuy <39868850+luongtrantronghuy@users.noreply.github.com>
2021-07-22 15:24:36 -0700

    ViewMessage ViewMention work now. Small quality of life changes to backend

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -6,15 +6,15 @@
 --   "content": "string"
 --   "from": "string"
 --   "to": "string"
---   "sent": "2020-11-17T23:17:19Z"
+--   "sent": "2019-11-17T23:17:19Z"
 -- }
 
 DELETE FROM channels;
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "First message in the asgn1 channel!","from": "molly","to": "assignment-1","sent": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "First message in all threads!","from": "anna","to": "","sent": "2020-11-17T23:17:19Z"}', '{"content": "Second message in the thread!","from": "anna","to": "","sent": "2021-01-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "Second message in the asgn1 channel @anna! This is a long long long long long long long long long long long long long long long long long long long long long long long long long text","from": "anna","to": "assignment-1","sent": "2020-11-17T23:17:19Z"}', ARRAY ['{"content": "This message can have threads too!","from": "anna","to": "","sent": "2021-01-17T23:17:19Z"}', '{"content": "Yes","from": "molly","to": "","sent": "2021-01-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "First message in the asgn2 channel!","from": "anna","to": "assignment-2","sent": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "Thread in assignment-2!","from": "molly","to": "","sent": "2020-10-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "This message was sent on same day!","from": "anna","to": "assignment-2","sent": "2020-10-17T23:17:19Z"}', ARRAY ['{"content": "Thread in assignment-2 ^^","from": "molly","to": "","sent": "2020-10-17T23:17:19Z"}']);
-INSERT INTO channels(channel, messages, thread) VALUES ('different-channel','{"content": "Different Channel","from": "anna","to": "different-channel","sent": "2020-10-17T23:17:19Z"}', ARRAY ['']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "First message in the asgn1 channel!","from": "molly","to": "assignment-1","sent": "2019-10-17T23:17:19Z"}', ARRAY ['{"content": "First message in all threads!","from": "anna","to": "","sent": "2019-11-17T23:17:19Z"}', '{"content": "Second message in the thread!","from": "anna","to": "","sent": "2020-01-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-1','{"content": "Second message in the asgn1 channel @anna! This is a long long long long long long long long long long long long long long long long long long long long long long long long long text","from": "anna","to": "assignment-1","sent": "2019-11-17T23:17:19Z"}', ARRAY ['{"content": "This message can have threads too!","from": "anna","to": "","sent": "2020-01-17T23:17:19Z"}', '{"content": "Yes","from": "molly","to": "","sent": "2020-01-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "First message in the asgn2 channel!","from": "anna","to": "assignment-2","sent": "2019-10-17T23:17:19Z"}', ARRAY ['{"content": "Thread in assignment-2!","from": "molly","to": "","sent": "2019-10-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('assignment-2','{"content": "This message was sent on same day!","from": "anna","to": "assignment-2","sent": "2019-10-17T23:17:19Z"}', ARRAY ['{"content": "Thread in assignment-2 ^^","from": "molly","to": "","sent": "2019-10-17T23:17:19Z"}']);
+INSERT INTO channels(channel, messages, thread) VALUES ('different-channel','{"content": "Different Channel","from": "anna","to": "different-channel","sent": "2019-10-17T23:17:19Z"}', ARRAY ['']);
 
 -- User Table --
 -- info:
@@ -38,6 +38,6 @@ INSERT INTO channelAccess(title, users) VALUES ('assignment-2', ARRAY ['anna']);
 INSERT INTO channelAccess(title, users) VALUES ('different-channel', ARRAY ['anna']);
 
 DELETE FROM dm;
-INSERT INTO dm(from_user, to_user, content, sent_at, thread) VALUES ('molly', 'anna', 'Hey! How are you doing?', '2020-10-17T23:17:19Z', ARRAY ['']);
-INSERT INTO dm(from_user, to_user, content, sent_at, thread) VALUES ('anna', 'molly', 'Not bad, wbu', '2021-10-17T23:17:19Z', ARRAY ['{"content": "Thread here!","from_user": "anna","to_user": "molly","sent_at": "2020-10-17T23:17:19Z"}', '{"content": "Thread here too!","from_user": "molly","to_user": "anna","sent_at": "2021-10-17T23:17:19Z"}']);
-INSERT INTO dm(from_user, to_user, content, sent_at, thread) VALUES ('anna', 'mollysister', 'Hi Molly Sister', '2021-10-17T23:17:19Z', ARRAY ['']);
+INSERT INTO dm(from_user, to_user, content, sent_at, thread) VALUES ('molly', 'anna', 'Hey! How are you doing?', '2019-10-17T23:17:19Z', ARRAY ['']);
+INSERT INTO dm(from_user, to_user, content, sent_at, thread) VALUES ('anna', 'molly', 'Not bad, wbu', '2020-10-17T23:17:19Z', ARRAY ['{"content": "Thread here!","from_user": "anna","to_user": "molly","sent_at": "2019-10-17T23:17:19Z"}', '{"content": "Thread here too!","from_user": "molly","to_user": "anna","sent_at": "2020-10-17T23:17:19Z"}']);
+INSERT INTO dm(from_user, to_user, content, sent_at, thread) VALUES ('anna', 'mollysister', 'Hi Molly Sister', '2020-10-17T23:17:19Z', ARRAY ['']);
--- frontend/src/ViewSearch.js
+++ frontend/src/ViewSearch.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import {fetchSearch} from './Fetcher';
-import {makeStyles, TextField} from '@material-ui/core';
+import {makeStyles, TableBody, TableRow, TextField} from '@material-ui/core';
+import {ListItem, Divider, List} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  notfound: {
+    color: 'darkgrey',
+  },
+  time: {
+    color: 'grey',
+  },
+  from: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  root: {
+    height: 'auto',
+    maxWidth: '100hv',
+    backgroundColor: theme.palette.background.paper,
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    overflowY: 'scroll',
+    marginLeft: '5%',
+    // marginRight: '10%',
+    width: '90%',
+  },
   inputField: {
     width: '100%',
     maxWidth: '1000px',
@@ -18,6 +40,52 @@ const useStyles = makeStyles((theme) => ({
     height: 'auto',
   },
 }));
+
+/**
+ *
+ * @param {*} props
+ * @return {object}
+ */
+function ListResult(props) {
+  const classes = useStyles();
+  return (
+    <>
+      {props.messages.map((message) => {
+        console.log(message);
+        if (message) {
+          let thread = <></>;
+          if (message.thread.length > 0) {
+            thread =
+            <>
+              <ListItem>Thread:</ListItem>
+              <List>
+                {message.thread.map((thread) => {
+                  return (
+                    <ListItem>{thread.content}</ListItem>
+                  );
+                })}
+              </List>
+            </>;
+          }
+          return (
+            <List>
+              <div className={message.found ? classes.found : classes.notfound}>
+                <span>{message.content}</span>
+                <span
+                  className={classes.time}
+                > at: {message.sent ? message.sent : message.sent_at}</span>
+              </div>
+              <div>{thread}</div>
+            </List>
+          );
+        }
+        return (
+          <></>
+        );
+      })}
+    </>
+  );
+}
 
 /**
  * Component for list of all messages @ our user
@@ -59,6 +127,23 @@ function Search(props) {
         </form>
       </div>
       <div>{error}</div>
+      <List className={classes.root}>
+        <TableBody>
+          {messages.map((message) => {
+            return (
+              <>
+                <TableRow className={classes.from}>
+                  In: {message.in}
+                </TableRow>
+                <Divider/>
+                <ListResult
+                  messages={message.messages}
+                />
+              </>
+            );
+          })}
+        </TableBody>
+      </List>
     </>
   );
 }
